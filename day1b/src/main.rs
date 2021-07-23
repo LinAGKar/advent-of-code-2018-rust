141b727736a6306cf5a53168296d0409e0ce6227
Linus Kardell <linus.kardell@gmail.com>
2021-07-23 23:15:23 +0200

    Optimize day1b

--- day1b/src/main.rs
+++ day1b/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::io;
 use std::io::Read;
 
@@ -7,15 +7,24 @@ fn main() -> io::Result<()> {
     io::stdin().read_to_string(&mut input)?;
     let frequency_changes: Vec<_> = input.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
     let mut frequency = 0;
-    let mut seen_frequencies = HashSet::new();
+    let mut seen_frequencies: VecDeque<_> = [true].iter().copied().collect();
+    let mut start = 0;
     loop {
         for i in &frequency_changes {
-            seen_frequencies.insert(frequency);
             frequency += i;
-            if seen_frequencies.contains(&frequency) {
+            while frequency < start {
+                seen_frequencies.push_front(false);
+                start -= 1;
+            }
+            let pos = (frequency - start) as usize;
+            if pos >= seen_frequencies.len() {
+                seen_frequencies.resize(pos + 1, false);
+            }
+            if seen_frequencies[pos] {
                 println!("{}", frequency);
                 return Ok(());
             }
+            seen_frequencies[pos] = true;
         }
     }
 }
