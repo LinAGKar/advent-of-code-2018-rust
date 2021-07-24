45c512d5424222e470d183e8bf13cb5f795e65b2
Linus Kardell <linus.kardell@gmail.com>
2021-07-24 23:11:34 +0200

    Optimize day 9

--- day9a/src/main.rs
+++ day9a/src/main.rs
@@ -1,166 +1,34 @@
-use std::cell::RefCell;
-use std::io;
-use std::rc::{Rc, Weak};
-
-// Severely Incomplete and in some ways broken (e.g. head and tail are not updated by
-// insert_after or pop_before) circular doubly linked list, just enough for this task
-struct Node<T> {
-    prev: Option<Weak<RefCell<Node<T>>>>,
-    next: Option<Rc<RefCell<Node<T>>>>,
-    value: T,
-}
-
-struct CircularList<T> {
-    head: Option<Rc<RefCell<Node<T>>>>,
-    tail: Option<Rc<RefCell<Node<T>>>>,
-}
-
-struct CircularListIterator<T> {
-    item: Rc<RefCell<Node<T>>>,
-}
-
-impl<T> CircularList<T> {
-    fn new() -> CircularList<T> {
-        CircularList {
-            head: None,
-            tail: None,
-        }
-    }
-
-    fn push_front(&mut self, value: T) {
-        let item = Rc::new(RefCell::new(Node {
-            prev: None,
-            next: None,
-            value: value,
-        }));
-        if let (Some(head), Some(tail)) = (&mut self.head, &self.tail) {
-            item.borrow_mut().prev = Some(Rc::downgrade(tail));
-            item.borrow_mut().next = Some(Rc::clone(head));
-            head.borrow_mut().prev = Some(Rc::downgrade(&item));
-            self.head = Some(Rc::clone(&item));
-        } else {
-            item.borrow_mut().prev = Some(Rc::downgrade(&item));
-            item.borrow_mut().next = Some(Rc::clone(&item));
-            self.head = Some(Rc::clone(&item));
-            self.tail = Some(Rc::clone(&item));
-        }
-    }
-
-    fn iter(&self) -> Option<CircularListIterator<T>> {
-        if let Some(head) = &self.head {
-            Some(CircularListIterator {
-                item: Rc::clone(head),
-            })
-        } else {
-            None
-        }
-    }
-}
-
-impl<T> CircularListIterator<T> {
-    fn next(&mut self) -> Option<()> {
-        let new_rc = if let Some(next) = &self.item.borrow().next {
-            Rc::clone(next)
-        } else {
-            return None;
-        };
-        self.item = new_rc;
-        Some(())
-    }
-
-    fn prev(&mut self) -> Option<()> {
-        let new_rc = if let Some(prev) = &self.item.borrow().prev {
-            prev.upgrade()?
-        } else {
-            return None;
-        };
-        self.item = new_rc;
-        Some(())
-    }
-
-    fn insert_after(&mut self, value: T) -> Option<()> {
-        let next_rc = if let Some(next) = &self.item.borrow().next {
-            Rc::clone(next)
-        } else {
-            return None;
-        };
-        let this_rc = Rc::downgrade(&self.item);
-        let item = Rc::new(RefCell::new(Node {
-            prev: Some(this_rc),
-            next: Some(next_rc),
-            value: value,
-        }));
-        let mut was_single_node = false;
-        let mut active_ref = self.item.borrow_mut();
-        if let Some(next) = &active_ref.next {
-            if let Ok(mut next_node) = next.try_borrow_mut() {
-                next_node.prev = Some(Rc::downgrade(&item));
-            } else {
-                was_single_node = true;
-            }
-        } else {
-            return None;
-        };
-        if was_single_node {
-            active_ref.prev = Some(Rc::downgrade(&item));
-        }
-        active_ref.next = Some(Rc::clone(&item));
-        Some(())
-    }
-
-    fn pop_before(&mut self) -> Option<T> {
-        let prev_rc = if let Some(prev) = &self.item.borrow().prev {
-            prev.upgrade().unwrap()
-        } else {
-            return None;
-        };
-        if Rc::ptr_eq(&prev_rc, &self.item) {
-            return None;
-        }
-        let before_prev_rc = if let Some(before_prev) = &prev_rc.borrow().prev {
-            before_prev.upgrade().unwrap()
-        } else {
-            return None;
-        };
-        before_prev_rc.borrow_mut().next = Some(Rc::clone(&self.item));
-        self.item.borrow_mut().prev = Some(Rc::downgrade(&before_prev_rc));
-        if let Ok(node) = Rc::try_unwrap(prev_rc) {
-            Some(node.into_inner().value)
-        } else {
-            None
-        }
-    }
-}
-
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    std::io::stdin().read_line(&mut input).unwrap();
     let mut words = input.split_whitespace();
     let player_count: usize = words.next().unwrap().parse().unwrap();
-    let end_value: u32 = words.nth(5).unwrap().parse().unwrap();
+    let end_value = words.nth(5).unwrap().parse::<u32>().unwrap();
 
-    let mut player_points = vec![0; player_count];
-    let mut current_player = 0;
+    let mut marbles = vec![(0u32, 0u32); end_value as usize + 1];
+    let mut points = vec![0; player_count];
+    let mut active = 0;
 
-    let mut current_value = 0;
-    let mut circle = CircularList::<u32>::new();
-    circle.push_front(current_value);
-    current_value += 1;
-    let mut active = circle.iter().unwrap();
-    while current_value <= end_value {
-        if current_value % 23 == 0 {
-            player_points[current_player] += current_value;
+    for (value, player) in (1..=end_value).zip((0..player_count).cycle()) {
+        if value % 23 == 0 {
+            points[player] += value;
             for _ in 0..6 {
-                active.prev().unwrap();
+                active = marbles[active as usize].0;
             }
-            player_points[current_player] += active.pop_before().unwrap();
+            points[player] += marbles[active as usize].0;
+            let before = (0..2).fold(active, |acc, _| marbles[acc as usize].0);
+            marbles[active as usize].0 = before;
+            marbles[before as usize].1 = active;
         } else {
-            active.next().unwrap();
-            active.insert_after(current_value).unwrap();
-            active.next().unwrap();
+            let before = marbles[active as usize].1;
+            let after = marbles[before as usize].1;
+            marbles[before as usize].1 = value;
+            marbles[after as usize].0 = value;
+            marbles[value as usize].0 = before;
+            marbles[value as usize].1 = after;
+            active = value;
         }
-        current_value += 1;
-        current_player = (current_player + 1) % player_count;
     }
-    println!("{}", player_points.into_iter().max().unwrap());
+
+    println!("{}", points.into_iter().max().unwrap());
 }
